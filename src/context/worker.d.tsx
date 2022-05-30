a281173d21f2f5c2ee64f6cd8c5ccae90fffcfef
JamesDinding <h0975627278gmail.com>
2022-05-30 16:38:40 +0800

    修改RecommendationBlock的Items宣告的型態，看起來是都會回傳array包起來的資料

--- src/context/worker.d.tsx
+++ src/context/worker.d.tsx
@@ -1,37 +1,33 @@
-
-
 declare type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 500;
 
 declare interface WorkerResponse {
-    status: HttpStatusCode;
-    msg: string;
+  status: HttpStatusCode;
+  msg: string;
 
-    imageblob?: string;
-    data?: any
-    blocks?: Array<RecommendationBlock>;
+  imageblob?: string;
+  data?: any;
+  blocks?: Array<RecommendationBlock>;
 }
 
 declare interface WorkerTask {
-    action: string; // 執行方法
-    data?: any;
+  action: string; // 執行方法
+  data?: any;
 }
 
 declare interface WorkerContextType {
-    send(msg: WorkerTask): Promise<WorkerResponse>;
+  send(msg: WorkerTask): Promise<WorkerResponse>;
 }
 
 // ----
 
 declare interface RecommendationBlock {
-    ID: number
-    Name: string
-    Items: Book
+  ID: number;
+  Name: string;
+  Items: Array<Book>; // Ding: 我這邊有改成Array<> ，這邊看起來是都會回傳用array包起來的資料
 }
 
 declare interface Book {
-    ID: number
-    Cover: string
-    Name: string
+  ID: number;
+  Cover: string;
+  Name: string;
 }
-
-

51a80ae8e80c5494ce89f8dc4fb9ba361bc69305
JamesDinding <h0975627278gmail.com>
2022-06-06 18:47:55 +0800

    充值頁面修正

--- src/components/Profile/Services/Charge.tsx
+++ src/components/Profile/Services/Charge.tsx
@@ -1,23 +1,42 @@
 import { FunctionalComponent, h } from "preact";
 
 const salesList = [
-  { price: `50元`, coins: `5000+3000`, bonus: "多送30元", explain: "金幣" },
-  { price: `30元`, coins: `3000`, bonus: "多送30元", explain: "金幣" },
-  { price: `100元`, coins: `10000+8000`, bonus: "多送80元", explain: "金幣" },
+  { title: `50元`, price: `5000+3000`, bonus: "多送30元", explain: "金幣" },
+  { title: `30元`, price: `3000`, bonus: "多送30元", explain: "金幣" },
   {
-    price: `200元`,
-    coins: `20000+2000`,
+    title: `100元`,
+    price: `10000+8000`,
+    bonus: "多送80元",
+    explain: "金幣",
+    recommend: true,
+    hot: true,
+  },
+  {
+    title: `200元`,
+    price: `20000+2000`,
     bonus: "多送200元",
     explain: "金幣",
   },
-  { price: `VIP季卡`, coins: `188`, bonus: "", explain: "90天 免費看" },
-  { price: `VIP年卡`, coins: `359`, bonus: "", explain: "365天 免費看" },
+  {
+    title: `VIP季卡`,
+    price: `188`,
+    bonus: "",
+    explain: "90天 免費看",
+    hot: true,
+  },
+  {
+    title: `VIP年卡`,
+    price: `359`,
+    bonus: "",
+    explain: "365天 免費看",
+    hot: true,
+  },
 ];
 
 const Charge = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full bg-white rounded-2xl mt-4">
-      <div className="flex flex-col items-center pt-4">
+    <div className="flex flex-col justify-center items-center w-full bg-white rounded-2xl mt-4 px-4">
+      <div className="flex flex-col items-center pt-4 mb-2">
         <span>i 充值金幣 i</span>
         <span className="text-xs flex text-[#a8a8a8]">
           歡迎使用
@@ -27,29 +46,34 @@ const Charge = () => {
           微信
         </span>
       </div>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-between">
         {salesList.map((sale, index) => {
           return (
             <div
-              className="rounded-2xl w-[175px] h-[100px]  p-2.5 border-[1px]"
+              className="rounded-2xl min-h-[90px] w-[47%] p-2.5 border-[1px] charge-item my-2 charge-item"
               key={index}
             >
-              <div>{sale.price}</div>
+              {sale.recommend && <div className="charge-recommend">推薦</div>}
               <div>
-                {sale.coins}
+                {sale.title}
+                {sale.hot && <span className="charge-hot">熱銷</span>}
+              </div>
+              <div>
+                {sale.price}
                 <span className="text-gray-500 text-sm">{sale.explain}</span>
               </div>
               <div className="text-[#a8a8a8] text-xs">{sale.bonus}</div>
             </div>
           );
         })}
-        <span className="text-xs text-[#a8a8a8]">
-          有任何問題請聯繫
-          <div className="text-center">
-            <a>在線克服</a>
-          </div>
-        </span>
       </div>
+      <div className="charge-vip-hint">VIP用户可无限阅读所有作品！</div>
+      <span className="text-xs text-[#a8a8a8]">
+        有任何問題請聯繫
+        <div className="text-center">
+          <a>在線克服</a>
+        </div>
+      </span>
     </div>
   );
 };

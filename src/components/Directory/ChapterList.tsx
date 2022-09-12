81ba8328f08a7fe9b7ec1c6909278cd6e46c2083
JamesDinding <h0975627278gmail.com>
2022-09-12 18:40:56 +0800

    間距調整

--- src/components/Directory/ChapterList.tsx
+++ src/components/Directory/ChapterList.tsx
@@ -37,7 +37,7 @@ const ChapterList = () => {
 
   return (
     <>
-      <div className="chapter-title mb-2">章節</div>
+      <div className="chapter-title mb-2.5">章節</div>
       <div className="grid grid-cols-3 gap-2.5 justify-between">
         {!isPending &&
           fakeData?.map((list) => {

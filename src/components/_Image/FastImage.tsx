55a6b3ebc4184d33bb1e93dbe391ba22173e06de
JamesDinding <h0975627278gmail.com>
2022-09-23 15:06:42 +0800

    remove useWorker

--- src/components/_Image/FastImage.tsx
+++ src/components/_Image/FastImage.tsx
@@ -1,7 +1,6 @@
 import { h, FunctionalComponent } from "preact";
 import { StateUpdater, useEffect, useState } from "preact/hooks";
 import { useObserver } from "../../context/observer";
-import { useWorker } from "../../context/worker";
 
 import avifDecoder from "./avif-decode/index";
 import { imgFactory } from "./ImageFactory";
@@ -17,41 +16,35 @@ const FastImage: FunctionalComponent<ImageProps> = ({
   path,
   setParentPending,
 }) => {
-  const { send } = useWorker();
   const [imageBase64, setImageBase64] = useState("");
 
   const observer = useObserver();
   const { ref, isShown } = observer.observe();
 
   useEffect(() => {
-    (async () => {
-      /*******test start*******/
-      fetch("/assets/img/Test-avif/Mexico.avif").then(async (res) => {
-        const blob = await res.blob();
-        const temp = imgFactory.create(blob, "avif");
-        const imgBase = await temp?.decode();
-
-        setImageBase64(imgBase);
-
-        // const base64IMG = await avifDecoder(blob);
-      });
-      /*******test end*******/
-
-      if (!isShown) return;
-
-      let res = await send({
-        action: "GetResourceImage",
-        data: {
-          path: path,
-        },
-      });
-
-      if (res.imageblob !== undefined) {
-        const avifBase64Img = await avifDecoder(res.imageblob);
-        setImageBase64(avifBase64Img);
-        setParentPending(false);
-      }
-    })();
+    // (async () => {
+    //   /*******test start*******/
+    //   fetch("/assets/img/Test-avif/Mexico.avif").then(async (res) => {
+    //     const blob = await res.blob();
+    //     const temp = imgFactory.create(blob, "avif");
+    //     const imgBase = await temp?.decode();
+    //     setImageBase64(imgBase);
+    //     // const base64IMG = await avifDecoder(blob);
+    //   });
+    //   /*******test end*******/
+    //   if (!isShown) return;
+    //   let res = await send({
+    //     action: "GetResourceImage",
+    //     data: {
+    //       path: path,
+    //     },
+    //   });
+    //   if (res.imageblob !== undefined) {
+    //     const avifBase64Img = await avifDecoder(res.imageblob);
+    //     setImageBase64(avifBase64Img);
+    //     setParentPending(false);
+    //   }
+    // })();
   }, [isShown]);
 
   //   return <img src="/assets/img/Test-avif/Mexico.avif" alt={alt} ref={ref} />;

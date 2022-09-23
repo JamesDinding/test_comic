b3a4efb42ca4d15d152d72d0d2886fed8fd3fd1b
JamesDinding <h0975627278gmail.com>
2022-09-23 16:17:55 +0800

    分類頁

--- src/components/Home/CategoryItemList.tsx
+++ src/components/Home/CategoryItemList.tsx
@@ -1,15 +1,24 @@
 import { h, FunctionalComponent, Fragment } from "preact";
 import { route } from "preact-router";
+import BookList from "../_Book/List";
 
 interface CategoryItemListProps {
-    catID: Number
+  catID: Number;
 }
 
-const HomeCategoryItemList: FunctionalComponent<CategoryItemListProps> =
-    ({ catID }) => {
-        return (<>
-            {catID}
-        </>);
-    }
+const HomeCategoryItemList: FunctionalComponent<CategoryItemListProps> = ({
+  catID,
+}) => {
+  return (
+    <div className="mx-5">
+      <BookList
+        Items={[{ ID: 123, Cover: "", Name: "" }]}
+        ItemPerRow={3}
+        type={"separate"}
+        isTemp={true}
+      />
+    </div>
+  );
+};
 
 export default HomeCategoryItemList;

bca0ff830e6d50c6f44d417fd8f98acad1b4743c
JamesDinding <h0975627278gmail.com>
2022-09-23 15:08:53 +0800

    remove unuse worker declaration

--- src/components/_Image/image.tsx
+++ src/components/_Image/image.tsx
@@ -2,8 +2,6 @@ import { h, FunctionalComponent } from "preact";
 import { StateUpdater, useEffect, useState } from "preact/hooks";
 import { useObserver } from "../../context/observer";
 
-import { useWorker } from "../../context/worker";
-
 interface ImageProps {
   alt: string;
   path: string;
@@ -15,28 +13,25 @@ const Image: FunctionalComponent<ImageProps> = ({
   path,
   setParentPending,
 }) => {
-  const { send } = useWorker();
   const [imageBlob, setImageBlob] = useState("");
 
   const observer = useObserver();
   const { ref, isShown } = observer.observe();
 
   useEffect(() => {
-    (async () => {
-      if (!isShown) return;
-
-      let res = await send({
-        action: "GetResourceImage",
-        data: {
-          path: path,
-        },
-      });
-
-      if (res.imageblob !== undefined) {
-        setImageBlob(res.imageblob);
-        setParentPending(false);
-      }
-    })();
+    // (async () => {
+    //   if (!isShown) return;
+    //   let res = await send({
+    //     action: "GetResourceImage",
+    //     data: {
+    //       path: path,
+    //     },
+    //   });
+    //   if (res.imageblob !== undefined) {
+    //     setImageBlob(res.imageblob);
+    //     setParentPending(false);
+    //   }
+    // })();
   }, [isShown]);
 
   return <img src={imageBlob} alt={alt} ref={ref} />;

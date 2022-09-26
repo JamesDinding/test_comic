a3c1da3c98680a97aa6709a1e1a6992d9bcb7416
JamesDinding <h0975627278gmail.com>
2022-09-26 13:59:21 +0800

    搜尋列調整

--- src/components/Home/BrandBar.tsx
+++ src/components/Home/BrandBar.tsx
@@ -8,14 +8,16 @@ const HomeBrandBar: FunctionalComponent = () => {
       <a href="#">
         <Logo class="w-24" alt="女神漫画" />
       </a>
-      <a
-        href="/search"
-        class="search-box bg-[#f1f1f1] px-4 py-2.5 rounded-full grow text-xs text-gray-500 font-medium flex mx-3"
-      >
-        <span class="grow">更多分类</span>
-        <IconSearch class="text-gray-500 h-4" alt="搜索" />
-      </a>
-      <a href="/recharge" class="flex flex-col items-center">
+      <div className="relative grow px-3">
+        <input
+          placeholder="更多分类"
+          class="search-box bg-[#f1f1f1] w-full px-5 py-2.5 rounded-full text-xs text-gray-500 font-medium flex"
+        ></input>
+        <div className="absolute top-1/2 right-8 translate-y-[-50%]">
+          <IconSearch class="text-gray-500 h-4" alt="搜索" />
+        </div>
+      </div>
+      <a href="/charge" class="flex flex-col items-center">
         <img src="/assets/img/deposit.gif" className="w-6" alt="充值" />
         <span class="text-[#666666] text-xs">充值</span>
       </a>

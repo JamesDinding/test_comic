83cf5768bc752592254ecb78128c454934e8932f
JamesDinding <h0975627278gmail.com>
2022-10-12 17:32:25 +0800

    chpaterItem樣式調整

--- src/components/_Book/ChapterItem.tsx
+++ src/components/_Book/ChapterItem.tsx
@@ -28,18 +28,17 @@ const ChapterItem: FunctionalComponent<ChapterItem> = ({
       >
         {chapter.isLocked && <div className="chapter-item-backdrop"></div>}
         {chapter.isLocked && (
-          <div className="absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]">
+          <div className="absolute left-1/2 translate-x-[-50%] pt-[1.125rem]">
             <IconLock class="w-5 h-5" />
           </div>
         )}
-        <img src="" alt="" />
         <img
           src="/assets/img/test/Image.png"
           className="w-full h-full"
           alt=""
         />
-        <div className="item-overlay"></div>
-        <div className="absolute left-1/2 bottom-2.5 translate-x-[-50%] whitespace-nowrap text-sm text-white text-center w-[70px] h-[20px] leading-[20px]">
+        <div className="item-overlay-chapter"></div>
+        <div className="absolute left-1/2 bottom-[.25rem] translate-x-[-50%] whitespace-nowrap text-sm text-white text-center w-[70px] h-[20px] leading-[20px]">
           名稱最長五
         </div>
       </div>

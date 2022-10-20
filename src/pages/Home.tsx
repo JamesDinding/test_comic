5c4e7a062f4ff0b5dcf5d75ea51a0e6a5d25b696
JamesDinding <h0975627278gmail.com>
2022-10-20 11:26:14 +0800

    修正首頁分類列表最後一項會觸發err

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,7 @@ import BrandBar from "../components/Home/BrandBar";
 import Recommend from "../components/Home/Recommend";
 import CategoryItemList from "../components/Home/CategoryItemList";
 import BookList from "../components/_Book/List";
-import { getDomains } from "../lib/api";
 import { ObserverProvider } from "../context/observer";
-import { useDomain } from "../context/domain";
 import useRequest from "../hooks/use-request";
 import { getCategories } from "../lib/api";
 import SmartBanner from "../components/SmartBanner";
@@ -16,50 +14,61 @@ import SmartBanner from "../components/SmartBanner";
 import FooterBar from "../components/FooterBar";
 
 const HomePage: FunctionalComponent = () => {
-  const { srcDomain } = useDomain();
   const [currentCategory, setCurrentCategory] = useState(0);
-  const [showSmartBanner, setShowSmartBanner] = useState(true)
+  const [showSmartBanner, setShowSmartBanner] = useState(true);
   const containerRef = useRef<HTMLDivElement>(null!);
-  const [categories, setCategories] = useState<Array<{name:string, id:number}>>([])
-  const [showSearch, setShowSearch] = useState(false)
-  const [searchResult, setSearchResult] = useState<Book[]>([])
+  const [categories, setCategories] = useState<
+    Array<{ name: string; id: number }>
+  >([]);
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchResult, setSearchResult] = useState<Book[]>([]);
 
-    useEffect(()=>{
-        try{
-            (async()=>{
-                const {data} = await getCategories();
-                setCategories(data)
-            })()
-        }catch(err:any){
-            console.log(err.message || 'failed')
-        }
-    }, [])
+  useEffect(() => {
+    try {
+      (async () => {
+        const { data } = await getCategories();
+        setCategories(data);
+      })();
+    } catch (err: any) {
+      console.log(err.message || "failed");
+    }
+  }, []);
 
   return (
     <>
-     {showSmartBanner ? (
-                <SmartBanner SetSmartBannerVisiblity={setShowSmartBanner} />
-              ) : (
-                <></>
-              )}
+      {showSmartBanner ? (
+        <SmartBanner SetSmartBannerVisiblity={setShowSmartBanner} />
+      ) : (
+        <></>
+      )}
 
       <div class="grow overflow-hidden overflow-y-auto" ref={containerRef}>
         <ObserverProvider rootElement={containerRef}>
-          <BrandBar onShowSearch={setShowSearch} onSearchResult={setSearchResult} />
-          <CategoryListBar onCategoryChanged={setCurrentCategory} categories={[{name:'首頁', id:0}].concat(categories)} />
+          <BrandBar
+            onShowSearch={setShowSearch}
+            onSearchResult={setSearchResult}
+          />
+          <CategoryListBar
+            onCategoryChanged={setCurrentCategory}
+            categories={[{ name: "首頁", id: 0 }].concat(categories)}
+          />
 
           <PullToRefresh containerElement={containerRef}>
             {currentCategory == 0 ? (
-              showSearch ? <div className="mx-5">
-              <BookList
-                Items={searchResult}
-                ItemPerRow={3}
-                type={"separate"}
-                isTemp={true}
-              />
-            </div> : <Recommend />
+              showSearch ? (
+                <div className="mx-5">
+                  <BookList
+                    Items={searchResult}
+                    ItemPerRow={3}
+                    type={"separate"}
+                    isTemp={true}
+                  />
+                </div>
+              ) : (
+                <Recommend />
+              )
             ) : (
-              <CategoryItemList catID={categories[currentCategory].id} />
+              <CategoryItemList catID={categories[currentCategory - 1].id} />
             )}
           </PullToRefresh>
         </ObserverProvider>

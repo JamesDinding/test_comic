ab154e87d7b21a165d012d1760ac666b8ee39967
JamesDinding <h0975627278gmail.com>
2022-11-09 16:43:02 +0800

    移除測試用的檔案

--- src/context/Memo-home/memeo.tsx
+++
@@ -1,9 +0,0 @@
-import { h, FunctionComponent, createContext } from "preact";
-import { useState, useEffect, useContext } from "preact/hooks";
-
-// storing home page data i.e. recommend, category, search & swiper
-const MemoContext = createContext({});
-
-// export const MemoProvider: FunctionalComponent = ({  }) => {
-//   const [] = useState();
-// };

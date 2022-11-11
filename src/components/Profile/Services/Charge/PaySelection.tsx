776309200435eca3f451fb46fb486f5c35a73aa0
Buwei Chiu <bu@x2d.cc>
2022-11-11 21:11:23 +0800

    點擊開發者模式要顯示的是金流 ID 而不是對應表 ID

--- src/components/Profile/Services/Charge/PaySelection.tsx
+++ src/components/Profile/Services/Charge/PaySelection.tsx
@@ -104,7 +104,7 @@ const PaySelection: FunctionalComponent<PaySelectionProps> = ({
                   name={"foo"}
                   value={selectName}
                 />
-                分流&nbsp;{i + 1}&nbsp;{dev_showId ? `(${p.id})` : ""}
+                分流&nbsp;{i + 1}&nbsp;{dev_showId ? `(${p.method_id})` : ""}
                 {p.bonus_text && (
                   <div className="inline text-left text-[#ff978d] leading-[10px] text-[10px] whitespace-nowrap">
                     {p.bonus_text}

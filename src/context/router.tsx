df9447320acea7a12af8345d0f3e2622581a3e56
JamesDinding <h0975627278gmail.com>
2022-11-11 12:24:09 +0800

    製作router相關component，嘗試解決uc瀏覽器issue

---
+++ src/context/router.tsx
@@ -0,0 +1,51 @@
+import { h, FunctionalComponent, createContext } from "preact";
+import { useState, useEffect, useContext, useCallback } from "preact/hooks";
+
+const RouterContext = createContext<RouterContextType>(null!);
+
+export const RouterProvider: FunctionalComponent = ({ children }) => {
+  const [currentUrl, setCurrentUrl] = useState(window.location.href || "");
+  const [routerStack, setRouterStack] = useState([window.location.href]);
+
+  const popHandler = useCallback(() => {
+    const temp = [...routerStack];
+    const popResult = temp.pop();
+
+    if (!popResult) return "";
+
+    setRouterStack(temp);
+    setCurrentUrl(popResult);
+    return popResult;
+  }, [routerStack]);
+
+  const pushHandler = useCallback((url: string, replace?: boolean) => {
+    const temp = [...routerStack];
+    setCurrentUrl(url);
+
+    if (replace) {
+      temp.pop();
+      temp.push(url);
+      setRouterStack(temp);
+    } else {
+      temp.push(url);
+      setRouterStack(temp);
+    }
+  }, []);
+
+  const value = {
+    currentRoute: currentUrl,
+    customRouter: {
+      routerStack,
+      pop: popHandler,
+      push: pushHandler,
+    },
+  };
+
+  return (
+    <RouterContext.Provider value={value}>{children}</RouterContext.Provider>
+  );
+};
+
+export function useUser() {
+  return useContext(RouterContext);
+}

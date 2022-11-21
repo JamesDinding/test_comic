a1a8a443d512c28f4dad73ccc759e17301fb0070
JamesDinding <h0975627278gmail.com>
2022-11-21 13:29:31 +0800

    收藏頁更改

--- src/components/_Book/CollectItem.tsx
+++ src/components/_Book/CollectItem.tsx
@@ -6,6 +6,7 @@ import { postMyBookmarks } from "../../lib/api";
 import { useUser } from "../../context/user";
 import { defaultLocalStorage } from "../../const";
 import { useRouter } from "../../context/router";
+import IconCross from "../../resources/img/icon-cross.svg";
 
 interface CollectItemProps {
   Data: Book;
@@ -15,7 +16,6 @@ interface CollectItemProps {
   updateList: StateUpdater<Array<Book>>;
 }
 
-let timer: ReturnType<typeof setTimeout>;
 const CollectItem: FunctionalComponent<CollectItemProps> = ({
   Data,
   index_temp,
@@ -33,7 +33,6 @@ const CollectItem: FunctionalComponent<CollectItemProps> = ({
   }, [curPress, index_temp]);
 
   function collectHandler(event: MouseEvent | TouchEvent) {
-    clearTimeout(timer);
     event.preventDefault();
     event.stopPropagation();
     if (!isLogIn) {
@@ -72,50 +71,30 @@ const CollectItem: FunctionalComponent<CollectItemProps> = ({
       id={(Data.ID || Data.id || "").toString()}
       class={"cursor-pointer item-separate "}
     >
-      <div class="relative h-[157px] rounded-lg overflow-hidden">
+      <div
+        class="relative h-[157px] rounded-lg overflow-hidden"
+        onClick={() => {
+          customRouter.push("/directory/" + Data.id);
+          route("/directory/" + Data.id);
+        }}
+      >
+        <div
+          className="absolute right-1 top-1 z-10 w-[25px] h-[25px] text-sm text-center text-white leading-[25px] rounded-full"
+          style={{ backgroundColor: "rgba(29,25,32,.6)" }}
+          onClick={collectHandler}
+        >
+          &#10005;
+        </div>
         <div
           className={
-            "duration-300 h-full " + (isLongPress ? "translate-y-[-30px]" : '"')
+            "duration-300 h-full " + (isLongPress ? "translate-y-[-30px]" : "")
           }
-          onMouseDown={(e) => {
-            e.preventDefault();
-            clearTimeout(timer);
-            setCurPress(index_temp);
-            timer = setTimeout(() => {
-              setIsLongPress(true);
-            }, 400);
-          }}
-          onMouseUp={(e) => {
-            clearTimeout(timer);
-            if (isLongPress) {
-              e.stopPropagation();
-              return;
-            }
-            customRouter.push("/directory/" + (Data.id || Data.ID));
-            route("/directory/" + (Data.id || Data.ID));
-          }}
-          onTouchStart={(e) => {
-            if (e.cancelable) e.preventDefault();
-            clearTimeout(timer);
-            setCurPress(index_temp);
-            timer = setTimeout(() => {
-              setIsLongPress(true);
-            }, 400);
-          }}
-          onTouchEnd={(e) => {
-            clearTimeout(timer);
-            if (isLongPress) {
-              e.stopPropagation();
-              return;
-            }
-            customRouter.push("/directory/" + (Data.id || Data.ID));
-            route("/directory/" + (Data.id || Data.ID));
-          }}
         >
-          {/* <div className={showPending ? "z-[20] pending h-[157px]" : ""}> */}
           <div
             className={
-              showPending ? "z-[20] h-full h-[157px] overflow-hidden" : "h-full"
+              showPending
+                ? "z-[20] h-full h-[157px] pending overflow-hidden"
+                : "h-full"
             }
           >
             <Image
@@ -123,16 +102,6 @@ const CollectItem: FunctionalComponent<CollectItemProps> = ({
               alt={Data.Name || Data.title || ""}
               setParentPending={setPending}
             />
-            {/* <img src="/assets/img/test/Image.png" className="" /> */}
-          </div>
-          <div
-            className="z-[30] bg-[#ff978d] text-center text-white text-sm font-light h-[30px] leading-[30px]"
-            onMouseDown={collectHandler}
-            onMouseUp={(e) => e.stopPropagation()}
-            onTouchStart={collectHandler}
-            onTouchEnd={(e) => e.stopPropagation()}
-          >
-            移除
           </div>
         </div>
       </div>
